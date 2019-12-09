f4e555a0c36259e8da8eae01a574ef3ed0358c36
Reza Hasani <reza.hasani7899@gmail.com>
2019-12-09 23:42:29 +0330

    try it now

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/App.css';
+import HomePage from "./views/HomePage";
+
+function App() {
+  return (
+      <HomePage />
+  );
+}
+
+export default App;
