c3c6232b00cd51226120008810f92ea4793bcb8f
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-10 05:16:16 +0330

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,11 @@ function Header() {
         <>
             <header className="header">
                 <div className="header__container">
+                    <img className="header__logo"/>
+                    <div className="header__details">
+                        <p className="header__slogan">Stage Is Ready</p>
+                        <p className="header__date">December 27th</p>
+                    </div>
                 </div>
             </header>
         </>
