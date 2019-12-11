d1e3fb32b0cceed740266deaa059d939d520542f
mohammad moallemi <mohammadmoallemi@outlook.com>
2019-12-11 22:48:43 +0330

    cant remember anything! might have fixed header! or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <>
             <header className="header">
                 <div className="header__container">
-                    <img className="header__logo" src={logo}/>
+                    <img className="header__logo"/>
                     <div className="header__details">
                         <p className="header__slogan">Stage Is Ready</p>
                         <p className="header__date">December 27th</p>
