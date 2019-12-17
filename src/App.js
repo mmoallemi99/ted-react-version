92de8e7c7499d7cd22904cfd4d1b0e1bacab61d8
Reza Hasani <reza.hasani7899@gmail.com>
2019-12-17 19:55:12 +0330

    FUCKING loader added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import HomePage from "./views/HomePage.jsx";
+import loader from "./assets/images/audio.svg";
+import './assets/styles/home-page.scss'
 
-function App() {
-  return (
-      <HomePage />
-  );
+class App extends React.Component {
+    state = {
+        loading: true
+    };
+
+    componentDidMount() {
+        // this simulates an async action, after which the component will render the content
+        demoAsyncCall().then(() => this.setState({loading: false}));
+    }
+
+    render() {
+        const {loading} = this.state;
+
+        if (loading) {
+            return (
+                // <div style={{height: "100vh", backgroundColor: "#2ecc71", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+                //     <div style={{color: "white", fontSize: "5rem"}}><span style={{color: "#ff2b06", fontWeight: "700"}}>TEDx</span> is loading</div>
+                //     <img src={loader} alt="" style={{width: "7vw", height: "50vh"}} />
+                // </div>
+                <div className="loader-container">
+                    <div className="loader"></div>
+                    <div className="loader-description"><span
+                        style={{color: "#ff2b06", fontWeight: "700"}}>TEDx</span> is Loading...
+                    </div>
+                    {/*// <div style={{color: "white", fontSize: "1rem"}}>Reza Hasani <span*/}
+                    {/*className="loader-creators-line">&</span> Farid Shokri <span*/}
+                    {/*className="loader-creators-line">&</span> Mohammad Moallemi</div>*/}
+                </div>
+            )
+        }
+
+        return (
+            <div><HomePage/></div>
+        );
+    }
+}
+
+function demoAsyncCall() {
+    return new Promise((resolve) => setTimeout(() => resolve(), 3000));
 }
 
 export default App;
